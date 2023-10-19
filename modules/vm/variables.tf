1dd227ef2456325b549cfc2849dbe622c96b1b2d
Yauheni Rykhter <ryhterrrr@mail.ru>
2023-10-19 20:09:15 +0400

    Update variables.tf

--- modules/vm/variables.tf
+++ modules/vm/variables.tf
@@ -2,8 +2,16 @@
 # Variables to be used with the infrastructure code
 ******************************/
 
-variable "gc_project" {}
-variable "gc_region" {}
+variable "gc_project" {
+  description = "Project"
+  type        = string
+}
+
+variable "gc_region" {
+  description = "Region"
+  type        = string
+}
+
 variable "instance_name" {}
 variable "instance_type" {}
 variable "label" {}
