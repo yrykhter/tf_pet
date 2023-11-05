b530410a2df78aeedf2de5434af6df94808505f4
Yauheni Rykhter <ryhterrrr@mail.ru>
2023-11-05 20:49:49 +0400

    add terratest file for all modules

---
+++ tests/all_code_test.go
@@ -0,0 +1,33 @@
+package test
+
+import (
+	"testing"
+
+	"github.com/gruntwork-io/terratest/modules/terraform"
+)
+
+func TestAllMpdulesTogether(t *testing.T) {
+	t.Parallel()
+
+	// Construct the terraform options with default retryable errors to handle the most common
+	// retryable errors in terraform testing.
+	terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
+		// The path to where our Terraform code is located
+		TerraformDir: "../",
+		Vars: map[string]interface{}{
+			"gc_project": "test",
+			"gc_region":  "us-central-1",
+			"env":        "dev",
+		},
+	})
+
+	// At the end of the test, run `terraform destroy` to clean up any resources that were created.
+	defer terraform.Destroy(t, terraformOptions)
+
+	// Run `terraform init` and `terraform apply`. Fail the test if there are any errors.
+	terraform.InitAndApply(t, terraformOptions)
+
+	// Run `terraform output` to get the IP of the instance
+	terraform.Output(t, terraformOptions, "ip_address")
+
+}
